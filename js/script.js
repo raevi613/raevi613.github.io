87159e5a75c0cf705b134f63c06634fb1e9af631
raevi613 <781824463@qq.com>
2022-03-18 13:41:44 +0800

    Site updated: 2022-03-18 13:41:43

--- js/script.js
+++ js/script.js
@@ -1,135 +1,120 @@
-(function (a) {
-  "use strict";
+window.addEventListener("DOMContentLoaded", function() {
+  const html            = document.querySelector("html");
+  const navBtn          = document.querySelector(".navbar-btn");
+  const navList         = document.querySelector(".navbar-list");
+  const backToTopFixed  = document.querySelector(".back-to-top-fixed");
+  let lastTop           = 0;
+  let theme             = window.localStorage.getItem('theme') || '';
 
-  var appendStylesheet = function (url) {
-    var link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = url;
-    document.body.appendChild(link);
-  };
+  theme && html.classList.add(theme)
 
-  appendStylesheet('//cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.css');
-
-  var $ = a;
-  $('.post-block p img').each(function () {
-    $(this).wrap('<a data-fancybox="images" href="' + this.src + '" data-caption=" ' + this.alt + '" class="fancybox"></a>');
-    $(this).attr('src', this.src + '?imageMogr2/thumbnail/300x300/gravity/North/crop/200x200');
-  });
-
-  if ($.fancybox) {
-    $('.fancybox').fancybox({
-      thumbs: {
-        // showOnStart: true
-      },
-      caption: function (instance, item) {
-        var caption, link;
-        if (item.type === 'image') {
-          caption = $(this).data('caption');
-          link = '<a href="' + item.src + '">Download</a>';
-          return (caption ? caption : '') + link;
-        }
-      },
-    });
-  }
-
-  // if ($.fancybox) {
-  //   $('.post-block p img')
-  //     .on('click', function () {
-  //       $.fancybox.open([{
-  //         src: this.src,
-  //         opts: {
-  //           caption: '<a href="' + this.src + '" target="_blank" download>Download</a>'
-  //         }
-  //       }]);
-  //     });
-  //   // $.fancybox.defaults.speed = 1000;
-  // }
-
-
-  function b(a) {
-    var c, d, b = [];
-    if ("number" == typeof a) b.push(a);
-    else {
-      d = a.split(",");
-      for (var e = 0; e < d.length; e++)
-        if (c = d[e].split("-"), 2 === c.length)
-          for (var f = parseInt(c[0], 10); f <= c[1]; f++) b.push(f);
-        else 1 === c.length && b.push(parseInt(c[0], 10))
+  const goScrollTop = () => {
+    let currentTop = getScrollTop()
+    let speed = Math.floor(-currentTop / 10)
+    if (currentTop > lastTop) {
+      return lastTop = 0
     }
-    return b
+    let distance = currentTop + speed;
+    lastTop = distance;
+    document.documentElement.scrollTop = distance;
+    distance > 0 && window.requestAnimationFrame(goScrollTop)
   }
-  a.fn.gist = function (c) {
-    return this.each(function () {
-      var e, f, g, h, i, j, k, l, m, n, d = a(this),
-        o = {};
-      return d.css("display", "block"), e = d.data("gist-id") || "", g = d.data("gist-file"), k = d.data("gist-hide-footer") === !0, l = d.data("gist-hide-line-numbers") === !0, h = d.data("gist-line"), j = d.data("gist-highlight-line"), n = d.data("gist-show-spinner") === !0, m = n ? !1 : void 0 !== d.data("gist-show-loading") ? d.data("gist-show-loading") : !0, g && (o.file = g), e ? (f = "https://gist.github.com/" + e + ".json", i = "Loading gist " + f + (o.file ? ", file: " + o.file : "") + "...", m && d.html(i), n && d.html('<img style="display:block;margin-left:auto;margin-right:auto"  alt="' + i + '" src="https://assets-cdn.github.com/images/spinners/octocat-spinner-32.gif">'), void a.ajax({
-        url: f,
-        data: o,
-        dataType: "jsonp",
-        timeout: 2e4,
-        success: function (c) {
-          var e, g, i, m, n;
-          c && c.div ? (c.stylesheet && (0 === c.stylesheet.indexOf("<link") ? c.stylesheet = c.stylesheet.replace(/\\/g, "").match(/href=\"([^\s]*)\"/)[1] : 0 !== c.stylesheet.indexOf("http") && (0 !== c.stylesheet.indexOf("/") && (c.stylesheet = "/" + c.stylesheet), c.stylesheet = "https://gist.github.com" + c.stylesheet)), c.stylesheet && 0 === a('link[href="' + c.stylesheet + '"]').length && (e = document.createElement("link"), g = document.getElementsByTagName("head")[0], e.type = "text/css", e.rel = "stylesheet", e.href = c.stylesheet, g.insertBefore(e, g.firstChild)), n = a(c.div), n.removeAttr("id"), d.html("").append(n), j && (m = b(j), n.find("td.line-data").css({
-            width: "100%"
-          }), n.find(".js-file-line").each(function (b) {
-            -1 !== a.inArray(b + 1, m) && a(this).css({
-              "background-color": "rgb(255, 255, 204)"
-            })
-          })), h && (i = b(h), n.find(".js-file-line").each(function (b) {
-            -1 === a.inArray(b + 1, i) && a(this).parent().remove()
-          })), k && (n.find(".gist-meta").remove(), n.find(".gist-data").css("border-bottom", "0px"), n.find(".gist-file").css("border-bottom", "1px solid #ddd")), l && n.find(".js-line-number").remove()) : d.html("Failed loading gist " + f)
-        },
-        error: function (a, b) {
-          d.html("Failed loading gist " + f + ": " + b)
-        },
-        complete: function () {
-          "function" == typeof c && c()
-        }
-      })) : !1
-    })
-  }, a(function () {
-    a("[data-gist-id]").gist()
+
+  const toggleBackToTopBtn = (top) => {
+    top = top || getScrollTop()
+    if (top >= 100) {
+      backToTopFixed.classList.add("show")
+    } else {
+      backToTopFixed.classList.remove("show")
+    }
+  }
+
+  toggleBackToTopBtn()
+
+  // theme light click
+  document.querySelector('#theme-light').addEventListener('click', function () {
+    html.classList.remove('theme-dark')
+    html.classList.add('theme-light')
+    window.localStorage.setItem('theme', 'theme-light')
   })
 
-})(jQuery);
+  // theme dark click
+  document.querySelector('#theme-dark').addEventListener('click', function () {
+    html.classList.remove('theme-light')
+    html.classList.add('theme-dark')
+    window.localStorage.setItem('theme', 'theme-dark')
+  })
 
-const postContainerEl = document.querySelector('.prince-container')
+  // theme auto click
+  document.querySelector('#theme-auto').addEventListener('click', function() {
+    html.classList.remove('theme-light')
+    html.classList.remove('theme-dark')
+    window.localStorage.setItem('theme', '')
+  })
 
-// console.log(postContainerEl.getComputedStyle())
+  // mobile nav click
+  navBtn.addEventListener("click", function () {
+    html.classList.toggle("show-mobile-nav");
+    this.classList.toggle("active");
+  });
 
-setTocStyle = () => {
-  const tocEl = document.querySelector('.prince-container .toc')
-  const tocWidth = tocEl.getBoundingClientRect().width
-  const postWidth = document.querySelector('.prince-container').getBoundingClientRect().width
-  const bodyWidth = document.body.getBoundingClientRect().width
-  console.log('(bodyWidth - postWidth) / 2 < tocWidth', (bodyWidth - postWidth) / 2 < tocWidth, bodyWidth,  (bodyWidth - postWidth) / 2, tocWidth)
-  tocEl.style.left = `${(bodyWidth - postWidth) / 2 + postWidth + 20}px`
-  tocEl.style.visibility = (bodyWidth - postWidth) / 2 < tocWidth ? 'hidden' : 'visible'
-  
-}
-
-
-const throttle = (fn, interval = 100) => {
-  let timer = null
-
-  return function() {
-    if (timer) {
-      return
+  // mobile nav link click
+  navList.addEventListener("click", function (e) {
+    if (e.target.nodeName == "A" && html.classList.contains("show-mobile-nav")) {
+      navBtn.click()
     }
-    timer = setTimeout(() => {
-      fn()
-      clearTimeout(timer)
-      timer = null
-    }, interval)
-    
-  }
+  })
 
+  // click back to top
+  backToTopFixed.addEventListener("click", function () {
+    lastTop = getScrollTop()
+    goScrollTop()
+  });
+
+  window.addEventListener("scroll", function () {
+    toggleBackToTopBtn()
+  }, { passive: true });
+
+  /** handle lazy bg iamge */
+  handleLazyBG();
+});
+
+/**
+ * 获取当前滚动条距离顶部高度
+ *
+ * @returns 距离高度
+ */
+function getScrollTop () {
+  return window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
 }
 
-const handlerWindowResize = throttle(setTocStyle, 50)
+function querySelectorArrs (selector) {
+  return Array.from(document.querySelectorAll(selector))
+}
 
 
-document.addEventListener('DOMContentLoaded', setTocStyle)
-window.addEventListener('resize', handlerWindowResize)
+function handleLazyBG () {
+  const lazyBackgrounds = querySelectorArrs('[background-image-lazy]')
+  let lazyBackgroundsCount = lazyBackgrounds.length
+  if (lazyBackgroundsCount > 0) {
+    let lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function({ isIntersecting, target }) {
+        if (isIntersecting) {
+          let img = target.dataset.img
+          if (img) {
+            target.style.backgroundImage = `url(${img})`
+          }
+          lazyBackgroundObserver.unobserve(target)
+          lazyBackgroundsCount --
+        }
+        if (lazyBackgroundsCount <= 0) {
+          lazyBackgroundObserver.disconnect()
+        }
+      })
+    })
 
-
+    lazyBackgrounds.forEach(function(lazyBackground) {
+      lazyBackgroundObserver.observe(lazyBackground)
+    })
+  }
+}
